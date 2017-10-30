7bb3c946a2aa202833bec6b7682b64523037a23f
PratyushKGP <pratyushdps@gmail.com>
2017-10-30 19:07:25 +0530

    Update ex3.m

--- machine-learning-ex3/ex3/ex3.m
+++ machine-learning-ex3/ex3/ex3.m
@@ -18,7 +18,7 @@
 
 %% Initialization
 clear ; close all; clc
-
+a=0;
 %% Setup the parameters you will use for this part of the exercise
 input_layer_size  = 400;  % 20x20 Input Images of Digits
 num_labels = 10;          % 10 labels, from 1 to 10   
@@ -31,7 +31,7 @@ num_labels = 10;          % 10 labels, from 1 to 10
 
 % Load Training Data
 fprintf('Loading and Visualizing Data ...\n')
-
+fprintf('initializing process')
 load('ex3data1.mat'); % training data stored in arrays X, y
 m = size(X, 1);
 
